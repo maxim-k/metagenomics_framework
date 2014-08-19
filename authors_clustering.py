9f9d63077d365541911c9dead01dff2c6fba9030
maxim-k <kuleshov.max.v@gmail.com>
2014-08-19 18:39:34 +0400

    Authors list

--- authors_clustering.py
+++ authors_clustering.py
@@ -1,1 +1,17 @@
 __author__ = 'maximk'
+
+
+
+def get_authors_list(authors_dict):
+    authors_list = set()
+    for paper_authors in authors_dict:
+        authors_list.update(set('%s %s' % (author['first_name'], author['last_name']) for author in paper_authors))
+    authors_list = sorted(list(authors_list))
+    return authors_list
+
+
+def main():
+    return None
+
+if __name__ == '__main__':
+    main()
--- papers_processing.py
+++ papers_processing.py
@@ -1,2 +1,16 @@
 __author__ = 'maximk'
 
+import json
+import os
+
+import authors_clustering
+
+path_json = '/home/maximk/Work/metagenomics/metagenomics_papers.json'
+
+if os.path.isfile(path_json):
+    papers = json.load(open(path_json, 'r'))
+
+authors = [paper['authors'] for paper in papers]
+authors_list = authors_clustering.get_authors_list(authors)
+
+print('\n'.join(authors_list))
