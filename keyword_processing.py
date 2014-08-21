1caf2a99f9d0c62173b923b788de3696862938ce
maxim-k <kuleshov.max.v@gmail.com>
2014-08-21 19:40:13 +0400

    Keywords processing

--- keyword_processing.py
+++ keyword_processing.py
@@ -1,13 +1,21 @@
 __author__ = 'maximk'
 
+from collections import OrderedDict
 
 def get_keywords_list(authors_dict):
     keywords_list = []
     return keywords_list
 
 
-def get_keywords_rating(authors_dict, keywords_list):
+def get_keywords_rating(keywords_dict, keywords_list):
     keywords_rating = dict.fromkeys(keywords_list, 0)
+    for paper_keywords in keywords_dict:
+        for keyword in paper_keywords:
+            keyword = '%s %s' % (keyword['first_name'], keyword['last_name'])
+            keywords_rating[keyword] += 1
+
+    keywords_rating = OrderedDict(sorted(keywords_rating.items(), key=lambda t: t[1], reverse=True))
+
     return keywords_rating
 
 
