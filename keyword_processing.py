3abd932398f6101f4adc056a45c2d43847121ad8
maxim-k <kuleshov.max.v@gmail.com>
2014-08-21 19:33:11 +0400

    Keywords processing

---
+++ keyword_processing.py
@@ -0,0 +1,18 @@
+__author__ = 'maximk'
+
+
+def get_keywords_list(authors_dict):
+    keywords_list = []
+    return keywords_list
+
+
+def get_keywords_rating(authors_dict, keywords_list):
+    keywords_rating = dict.fromkeys(keywords_list, 0)
+    return keywords_rating
+
+
+def main():
+    return None
+
+if __name__ == '__main__':
+    main()
