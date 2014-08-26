e5c25ba59137e6d0079b3327fddf3e10a4b947be
maxim-k <kuleshov.max.v@gmail.com>
2014-08-26 18:53:21 +0400

    Comments

--- keywords_processing.py
+++ keywords_processing.py
@@ -4,6 +4,9 @@ from collections import OrderedDict
 
 
 def get_keywords_list(keywords_dict):
+    """
+    Get keywordss from Mendeley JSON; remove duplicates.
+    """
     keywords_list = set()
     for paper_keywords in keywords_dict:
         keywords_list.update(set(keyword for keyword in paper_keywords))
@@ -12,6 +15,9 @@ def get_keywords_list(keywords_dict):
 
 
 def get_keywords_ratings(keywords_dict, keywords_list):
+    """
+    Get  frequency of each keyword
+    """
     keywords_rating = dict.fromkeys(keywords_list, 0)
     for paper_keywords in keywords_dict:
         for keyword in paper_keywords:
